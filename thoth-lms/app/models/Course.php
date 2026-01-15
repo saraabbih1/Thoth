b9a70d00915da7911920497f2b93943b61ba89b0
saraabbih1 <abbihsara30@gmail.com>
2026-01-15 16:00:56 +0100

    feat:course.php

--- thoth-lms/app/models/Course.php
+++ thoth-lms/app/models/Course.php
@@ -1,1 +1,29 @@
-<?php 
+<?php
+
+class Course
+{
+    private $db;
+
+    public function __construct()
+    {
+        $database = new Database();
+        $this->db = $database->pdo;
+    }
+
+    public function getAll()
+    {
+        $sql = "SELECT * FROM courses";
+        $stmt = $this->db->query($sql);
+
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function findById($id)
+    {
+        $sql = "SELECT * FROM courses WHERE id = ?";
+        $stmt = $this->db->prepare($sql);
+        $stmt->execute([$id]);
+
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+}
